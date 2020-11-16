384af422e21441dd9f8de295bb21f811f4a09d97
Tony Jang <tony@chequer.io>
2020-11-16 17:55:46 +0900

    Add PrimarSql.Data project

---
+++ PrimarSql.Data.Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimarSql.Data.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var root = PrimarSqlParser.Parse("SELECT * FROM actor");
+        }
+    }
+}
