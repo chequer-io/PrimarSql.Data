d62223062c9bb91fe76d4e2d0e7cb860630f7ed4
Tony Jang <tony@chequer.io>
2020-11-21 12:34:40 +0900

    Add ValidateTableWithIndexName

--- PrimarSql.Data/Utilities/Validator.cs
+++ PrimarSql.Data/Utilities/Validator.cs
@@ -12,5 +12,14 @@ namespace PrimarSql.Data.Utilities
             if (values.Length > 1)
                 throw new InvalidOperationException("Table name should be single identifier");
         }
+        
+        public static void ValidateTableWithIndexName(string[] values)
+        {
+            if (values.Length == 0)
+                throw new InvalidOperationException("Empty Table name.");
+
+            if (values.Length > 2)
+                throw new InvalidOperationException("Identifiers is too long. (table_name[.index_name])");
+        }
     }
 }
