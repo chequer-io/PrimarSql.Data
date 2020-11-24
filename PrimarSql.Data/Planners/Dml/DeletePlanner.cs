7fe9ad3c0079a78723c6734fafcd0aa1f8dc2fc3
Tony Jang <tony@chequer.io>
2020-11-24 17:27:47 +0900

    Impl DeletePlanner

---
+++ PrimarSql.Data/Planners/Dml/DeletePlanner.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using PrimarSql.Data.Extensions;
+using PrimarSql.Data.Models.Columns;
+using PrimarSql.Data.Providers;
+using PrimarSql.Data.Sources;
+
+namespace PrimarSql.Data.Planners
+{
+    internal sealed class DeletePlanner : QueryPlanner<DeleteQueryInfo>
+    {
+        public DeletePlanner(DeleteQueryInfo queryInfo) : base(queryInfo)
+        {
+        }
+
+        public IColumn[] GetColumns()
+        {
+            var columns = new List<IColumn>();
+
+            var tableDescription = Context.GetTableDescription(QueryInfo.TableName);
+            var hashKeyName = tableDescription.KeySchema.First(schema => schema.KeyType == KeyType.HASH).AttributeName;
+            var sortKeyName = tableDescription.KeySchema.FirstOrDefault(schema => schema.KeyType == KeyType.RANGE)?.AttributeName;
+
+            columns.Add(new PropertyColumn(hashKeyName));
+
+            if (!string.IsNullOrEmpty(sortKeyName))
+                columns.Add(new PropertyColumn(sortKeyName));
+
+            return columns.ToArray();
+        }
+
+        public override DbDataReader Execute()
+        {
+            var selectQueryInfo = new SelectQueryInfo
+            {
+                WhereExpression = QueryInfo.WhereExpression,
+                Limit = QueryInfo.Limit,
+                TableSource = new AtomTableSource(QueryInfo.TableName, string.Empty),
+                Columns = GetColumns(),
+            };
+
+            var planner = new SelectPlanner
+            {
+                QueryInfo = selectQueryInfo,
+                Context = Context
+            };
+
+            var reader = planner.Execute();
+            bool hasSortKey = selectQueryInfo.Columns.Length == 2;
+            
+            while (reader.Read())
+            {
+                var request = new DeleteItemRequest
+                {
+                    TableName = QueryInfo.TableName,
+                    ExpressionAttributeNames = ExpressionAttributeNames,
+                    ExpressionAttributeValues = ExpressionAttributeValues
+                };
+
+                request.Key.Add(reader.GetName(0), reader[0].ToAttributeValue());
+
+                if (hasSortKey)
+                    request.Key.Add(reader.GetName(1), reader[1].ToAttributeValue());
+
+                try
+                {
+                    Context.Client.DeleteItemAsync(request).Wait();
+                }
+                catch (AggregateException e)
+                {
+                    var innerException = e.InnerExceptions[0];
+                    throw new Exception($"Error while delete Item (Key: {reader.GetName(0)}){Environment.NewLine}{innerException.Message}");
+                }
+            }
+
+            return new PrimarSqlDataReader(new EmptyDataProvider());
+        }
+    }
+}
---
+++ PrimarSql.Data/Planners/Dml/DeleteQueryInfo.cs
@@ -0,0 +1,13 @@
+﻿using PrimarSql.Data.Expressions;
+
+namespace PrimarSql.Data.Planners
+{
+    internal sealed class DeleteQueryInfo : IQueryInfo
+    {
+        public string TableName { get; set; }
+
+        public IExpression WhereExpression { get; set; }
+
+        public int Limit { get; set; } = -1;
+    }
+}
