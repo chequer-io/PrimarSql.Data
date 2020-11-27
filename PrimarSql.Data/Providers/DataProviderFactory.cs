7e23a9a9713e151161a3ec4563c9458c929755c4
Tony Jang <tony@chequer.io>
2020-11-28 02:52:06 +0900

    Add validate data source null

--- PrimarSql.Data/Providers/DataProviderFactory.cs
+++ PrimarSql.Data/Providers/DataProviderFactory.cs
@@ -1,4 +1,7 @@
-﻿using PrimarSql.Data.Models;
+﻿using System;
+using System.Linq;
+using PrimarSql.Data.Models;
+using PrimarSql.Data.Models.Columns;
 using PrimarSql.Data.Planners;
 using PrimarSql.Data.Sources;
 
@@ -13,7 +16,18 @@ namespace PrimarSql.Data.Providers
                 return new SubqueryProvider(context, queryInfo);
             }
 
-            return new ApiDataProvider(context, queryInfo);
+            if (queryInfo.TableSource is AtomTableSource)
+            {
+                return new ApiDataProvider(context, queryInfo);
+            }
+
+            // TODO: If column contains derived column (expression column is not supported yet)
+            if (queryInfo.Columns.Any(column => column is PropertyColumn || column is StarColumn))
+            {
+                throw new InvalidOperationException("Cannot provide data. Source is not defined.");
+            }
+            
+            return new EmptyDataProvider();
         }
     }
 }
